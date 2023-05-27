f15c40020b9c258dfc3386899ea54e15afdca57f
hajsdhjaduiwiezddawd <120689825+hajsdhjaduiwiezddawd@users.noreply.github.com>
2023-05-27 15:13:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-function nike(){document.removeEventListener("click",nike,null);let e=new Audio("messivideo.mp4");e.loop=!0;e.play()}
+function nike(){document.removeEventListener("click",nike,null);let e=new Audio("messi.mp4");e.loop=!0;e.play()}
 document.addEventListener("click",nike);
